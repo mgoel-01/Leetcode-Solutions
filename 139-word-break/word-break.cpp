a8b65e535561f9e3986e11c6728521ffc94e1296
mgoel-01 <goelmanthan03@gmail.com>
2026-09-07 19:51:12 +0530

    Time: 19 ms (14.41%) | Memory: 18.1 MB (17.22%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -14,7 +14,19 @@ public:
     }
     bool wordBreak(string s, vector<string>& wordDict) {
         int n=s.size();
-        vector<vector<int>> dp(n,vector<int>(n,-1));
-        return canSegmented(0,0,n,s,wordDict,dp);
+        vector<vector<bool>> dp(n+1,vector<bool>(n+1,false));
+        for(int j=0;j<=n;j++)dp[n][j]=true;
+        for(int i=n-1;i>=0;i--){
+            for(int j=n-1;j>=i;j--){
+                bool isTrue1=false;
+                string str=s.substr(i,j-i+1);
+                if(find(wordDict.begin(),wordDict.end(),str)!=wordDict.end()){
+                    isTrue1=dp[j+1][j+1];
+                }
+                bool isTrue2=dp[i][j+1];
+                dp[i][j]=isTrue1||isTrue2;
+            }
+        }
+        return dp[0][0];
     }
 };

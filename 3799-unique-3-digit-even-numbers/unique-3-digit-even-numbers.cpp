a14f01932bc918392fee1770ce243ab253e05b2c
mgoel-01 <goelmanthan03@gmail.com>
2026-09-11 06:09:21 +0530

    Time: 8 ms (56.12%) | Memory: 31 MB (33.29%) - LeetSync

---
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int totalNumbers(vector<int>& digits) {
+        int n=digits.size();
+        int ans=0;
+        unordered_set<int> st;
+        for(int i=0;i<n;i++){
+            if(digits[i]==0)continue;
+            for(int j=0;j<n;j++){
+                if(i==j)continue;
+                for(int k=0;k<n;k++){
+                    if(i==k || j==k)continue;
+                    if(digits[k]%2==0){
+                        st.insert((digits[i]*100)+(digits[j]*10)+digits[k]);
+                    }
+                }
+            }
+        }
+        return st.size();
+    }
+};

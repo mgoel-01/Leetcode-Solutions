c67fa323f633719589340a0bca44ed9e926a46d8
mgoel-01 <goelmanthan03@gmail.com>
2026-09-11 10:07:53 +0530

    Time: 47 ms (51.82%) | Memory: 66.3 MB (36.42%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
-    bool detectCycle(int node,vector<int>& vis,vector<int>& dfsVis,vector<vector<int>>& graph){
-        vis[node]=1;
-        dfsVis[node]=1;
-        for(auto it:graph[node]){
-            if(!vis[it]){
-                if(detectCycle(it,vis,dfsVis,graph))return true;
-            }
-            else if(dfsVis[it])return true;
-        }
-        dfsVis[node]=0;
-        return false;
-    }
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n=graph.size();
-        vector<int> vis(n,0),dfsVis(n,0);
+        vector<vector<int>> adjRev(n);
+        vector<int> indegree(n,0);
+        for(int i=0;i<n;i++){
+            for(auto it:graph[i]){
+                adjRev[it].push_back(i);
+                indegree[i]++;
+            }
+        }
+        queue<int> q;
         vector<int> ans;
         for(int i=0;i<n;i++){
-                if(!detectCycle(i,vis,dfsVis,graph)){
-                    ans.push_back(i);
-                }
+            if(indegree[i]==0)q.push(i);
         }
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            ans.push_back(node);
+            for(auto it:adjRev[node]){
+                indegree[it]--;
+                if(indegree[it]==0)q.push(it);
+            }
+        }
+        sort(ans.begin(),ans.end());
         return ans;
     }
 };

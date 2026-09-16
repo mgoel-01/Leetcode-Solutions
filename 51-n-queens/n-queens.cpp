c6f3a4ac2e25131d49158e36623f0a27ada4a0b5
mgoel-01 <goelmanthan03@gmail.com>
2026-09-16 08:05:13 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (77.64%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,38 +1,21 @@
 class Solution {
 public:
-    bool isSafe(int row,int col,vector<string>& board,int n){
-        int duprow=row;
-        int dupcol=col;
-        while(row>=0 && col>=0){
-            if(board[row][col]=='Q')return false;
-            row--;
-            col--;
-        }
-        row=duprow;
-        col=dupcol;
-        while(col>=0){
-            if(board[row][col]=='Q')return false;
-            col--;
-        }
-        row=duprow;
-        col=dupcol;
-        while(row<n && col>=0){
-            if(board[row][col]=='Q')return false;
-            row++;
-            col--;
-        }
-        return true;
-    }
-    void solver(int col,vector<string>&board,vector<vector<string>>& ans,int n){
+    void solver(int col,vector<string>&board,vector<vector<string>>& ans,vector<int>& leftRow,vector<int>& upperDiag,vector<int>& lowerDiag,int n){
         if(col==n){
             ans.push_back(board);
             return;
         }
         for(int row=0;row<n;row++){
-            if(isSafe(row,col,board,n)){
+            if(leftRow[row]==0 && lowerDiag[row+col]==0 && upperDiag[n-1+col-row]==0){
                 board[row][col]='Q';
-                solver(col+1,board,ans,n);
+                leftRow[row]=1;
+                lowerDiag[row+col]=1;
+                upperDiag[n-1+col-row]=1;
+                solver(col+1,board,ans,leftRow,upperDiag,lowerDiag,n);
                 board[row][col]='.';
+                leftRow[row]=0;
+                lowerDiag[row+col]=0;
+                upperDiag[n-1+col-row]=0;
             }    
         }
     }
@@ -41,7 +24,8 @@ public:
         vector<string> board(n);
         string s(n,'.');
         for(int i=0;i<n;i++)board[i]=s;
-        solver(0,board,ans,n);
+        vector<int> leftRow(n,0),upperDiag((2*n)-1,0),lowerDiag((2*n)-1,0);
+        solver(0,board,ans,leftRow,upperDiag,lowerDiag,n);
         return ans;
     }
 };
